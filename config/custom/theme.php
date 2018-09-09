4dac3680c675c967fc98418f433fbba55ef94768
Dima Minka <bzx@mail.ru>
2018-09-09 18:36:28 +0300

    fix--custom--default theme support errors

--- config/custom/theme.php
+++ config/custom/theme.php
@@ -1,5 +1,5 @@
 <?php
 // set default theme
-if (env('CDK_THEME') && is_dir(define('CONTENT_DIR', '/app') . '/themes/' . env('CDK_THEME'))) {
+if (env('CDK_THEME') && is_dir(WP_CONTENT_DIR . '/themes/' . env('CDK_THEME'))) {
     define('WP_DEFAULT_THEME', env('CDK_THEME'));
 }
