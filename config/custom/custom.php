3cf21c815a1701d17762978c7bef0bee3497c700
Dima Minka <bzx@mail.ru>
2019-01-16 16:42:20 +0200

    feat--custom-config--descriptions, gtm, cron

--- config/custom/custom.php
+++ config/custom/custom.php
@@ -5,11 +5,19 @@
 
 use Roots\WPConfig\Config;
 
+// Changing the Number of Revisions to Save for Posts
 Config::define('WP_POST_REVISIONS', 3);
+// Change the Autosave Interval
 Config::define('AUTOSAVE_INTERVAL', 86400);
-
+// Increasing the PHP Memory Limit
 Config::define('WP_MEMORY_LIMIT', env('CDK_MEMORY'));
+// Increase Wordpress Memory Limit
 Config::define('WP_MAX_MEMORY_LIMIT', env('CDK_MEMORY'));
-
+// Enabling the "Trash" Feature for Media Files
 Config::define('MEDIA_TRASH', true);
+// The length of days before items are permanently delete
 Config::define('EMPTY_TRASH_DAYS', 10);
+// Disable WordPress cron 
+Config::define('DISABLE_WP_CRON', true);
+// Define Google Tag Manager ID
+Config::define('WP_GTM', env('WP_GTM'));
